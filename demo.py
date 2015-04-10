2acc5ffe8d13f35ffa89dd048ea423d4f067f83c
LUOXIALANgit config --global user.email luoxia061091@gmail.com <luoxia061091@gmail.com>
2015-04-10 21:29:24 +0800

    Add comment

--- demo.py
+++ demo.py
@@ -1,24 +1,21 @@
 import json
 import random
 
-file = open("yelp_academic_dataset_business.json")
+file = open("dataset/yelp_academic_dataset_business.json")
 
-# parameters for input data
-data = ""
-dataList = [] 		# list to store the data
+dataList = []           # list to store the data
 
-# parameters for clusters
-k = 4
-mdThreshold = 0.34
-threshold = [6, 8, 9]
-resultList = []
-#mdResultList = []
-clusterIndex = 0
-clusterResult = {}
-centroidList = [] 	# expamle: [{'stars': 4.5, 'lati': 40.408735, 'id': u'mVHrayjG3uZ_RLHkLj-AMg', 'long': -79.8663507}]
-dsList = [] 		# expamle: [{'No':1, 'N': 10, 'SUM':[], 'SUMSQ':[], :, "Points": [] }]
+k = 4                   # the number of clusters
+mdThreshold = 0.34      # the threshold for MD
+threshold = [6, 8, 9]   # the threshold for Variance, example [1, 2, 3]
+
+resultList = []         # the calculated reslut: MD and Variance
+clusterResult = {}		# the result of clustering {type: 1, value: 1}, value means the index
+
+centroidList = []       # expamle: [{'stars': 4.5, 'lati': 40.408735, 'id': u'mVHrayjG3uZ_RLHkLj-AMg', 'long': -79.8663507}]
+dsList = []             # expamle: [{'No':1, 'N': 10, 'SUM':[], 'SUMSQ':[], :, "Points": [] }]
 csList = []
-rsList = []
+rsList = []				# expamle: [{'stars': 4.5, 'lati': 40.408735, 'id': u'mVHrayjG3uZ_RLHkLj-AMg', 'long': -79.8663507}]
 
 # read the file and transform into the standard format
 while 1:
@@ -26,10 +23,10 @@ while 1:
     if not lines:
         break 							# break the loop when finishing reading the file
     for line in lines:
-    	data = data, json.dumps(line) 	# combine the line
     	data_json = json.loads(line)  	# convert the str to json format
     	d = {"id":data_json["business_id"], "long":data_json["longitude"], "lati":data_json["latitude"],"stars":data_json["stars"]}
     	dataList.append(d) 				# add the dicr to the list
+print "item:",len(dataList)
 
 # get k centroids
 # function: check duplication in the centroid list 
@@ -48,32 +45,47 @@ while i < k:
 	if Duplication(centroidList, rd["id"]):
 		centroidList.append(rd)
 		i = len(centroidList)
-		longFloat = float(rd["long"])
 		latiFloat = float(rd["lati"])
+		longFloat = float(rd["long"])
 		starsFloat = float(rd["stars"])
 		dsList.append({"No":i, "N":1, "SUM":[latiFloat,longFloat,starsFloat],"SUMSQ":[latiFloat**2, longFloat**2,starsFloat**2], "Points":[rd]})
 		centroidList[len(centroidList)-1]["No"]= i
-
-print "item:",len(dataList)
 
 print "random centroids:"
 for i in range(0, len(centroidList), 1):
 	print centroidList[i]
 print ""
 
-#	print "DS set:"	
-# 	for i in range(0, len(centroidList), 1):
-#		print dsList[i]
-
+# clustering
 def getSUMSQ(d):
-	sumsq = float(d)**2
-	return sumsq
+	sq= float(d)**2
+	return sq
 
 def getVariance(l):
-	v = [0,0,0] 
-	v[0] = float(l["SUMSQ"][0])/l["N"] - (float(l["SUM"][0])/l["N"])**2
-	v[1] = float(l["SUMSQ"][1])/l["N"] - (float(l["SUM"][1])/l["N"])**2
-	v[2] = float(l["SUMSQ"][2])/l["N"] - (float(l["SUM"][2])/l["N"])**2
+	v = [] 
+	v.append(float(l["SUMSQ"][0])/l["N"] - (float(l["SUM"][0])/l["N"])**2)
+	v.append(float(l["SUMSQ"][1])/l["N"] - (float(l["SUM"][1])/l["N"])**2)
+	v.append(float(l["SUMSQ"][2])/l["N"] - (float(l["SUM"][2])/l["N"])**2)
+	return v
+
+def getCombinedVariance(o1, o2):
+
+	sq = []
+	s = []
+	v = []
+
+	sq.append(getSUMSQ(o1["lati"]) + float(o2["SUMSQ"][0]))
+	sq.append(getSUMSQ(o1["long"]) + float(o2["SUMSQ"][1]))
+	sq.append(getSUMSQ(o1["stars"]) + float(o2["SUMSQ"][2]))	
+	
+	s.append(float(o1["lati"]) + o2["SUM"][0])
+	s.append(float(o1["long"]) + o2["SUM"][1])
+	s.append(float(o1["stars"]) + o2["SUM"][2])
+	
+	v.append(float(sq[0])/2 - (float(s[0])/2)**2)
+	v.append(float(sq[1])/2 - (float(s[1])/2)**2)
+	v.append(float(sq[2])/2 - (float(s[2])/2)**2)
+
 	return v
 
 def getMahalanobis(l1, l2, v):
@@ -82,47 +94,87 @@ def getMahalanobis(l1, l2, v):
 		((float(l1["stars"]) - float(l2["stars"]))/(v[2]**0.5))**2)**0.5
 	return md
 
-def getTVariance(o1, o2):
+def getClusterIndex(l, mdThre, varThre, type):
+	index = -1
+	if type == 1:									# 1: all are variance
+		pr = True
+		for j in range(0, len(l), 1):
+			for i in range(0, len(l[j]["value"]), 1):
+				if l[j]["value"][i] > varThre[i]:
+					pr = False
+					break
+			if pr == True:							# If the point find a cluster, then stop. 
+				index = j
+				return index
+		return index
 
-	#print "orignal ds set:", 
-	#print o2
-	#print "The point to be check:"
-	#print o1
+	if type == 2:									# 2: all are md, 
+		result = mdThre
+		for z in range(0, len(l), 1):
+			if l[z]["value"] <= result:
+				index = z
+				result = l[z]["value"]
+		return index
 
-	sq = [0,0,0]
-	s = [0,0,0]
+	if type == 3:									# 3: some are md, some are variance
+		result = mdThre
+		for z in range(0, len(l), 1):
+			if l[z]["type"] == 2:					# check the cluster which number of points is bigger than 1 first
+				if l[z]["value"] <= result:
+					index = z
+					result = l[z]["value"]
+		
+		if index == -1:								# If we can not find a suitable cluster, then check the othet
+			r = True
+			for j in range(0, len(l), 1):
+				if l[j]["type"] == 1:
+					for i in range(0, len(l[j]["value"]), 1):
+						if l[j]["value"][i] > varThre[i]:
+							r = False
+							break
+					if r == True:					# If we can find a cluster, then stop
+						index = j
+						return index
+			return index
+		else:
+			return index
+		
+def getClusterResult(l, mdThre, varThre, k):
+	cr = {}							# the result of clustering				
+	case = 0 						
+	count = 0							
 	
-	sq[0] = getSUMSQ(o1["lati"]) + float(o2["SUMSQ"][0])
-	sq[1] = getSUMSQ(o1["long"]) + float(o2["SUMSQ"][1])
-	sq[2] = getSUMSQ(o1["stars"]) + float(o2["SUMSQ"][2])	
+	for i in range(0, len(l), 1):
+		if l[i]["type"] == 1: 		
+			count = count + 1 		# count the number of type 1
 	
-	#print "The SUMSQ aftering combine:",
-	#print sq
+	if count == k :
+		case = 1 					# 1: all are variance
+	if count == 0:
+		case = 2					# 2: all are md
+	if 0 < count < k:
+		case = 3 					# 3: some are md, some are variance
 	
-	s[0] = float(o1["lati"]) + o2["SUM"][0]
-	s[1] = float(o1["long"]) + o2["SUM"][1]
-	s[2] = float(o1["stars"]) + o2["SUM"][2]
-	#print "The SUM after combine:"
-	#print s
-	
-	v = [0,0,0]
-	v[0] = float(sq[0])/2 - (float(s[0])/2)**2
-	v[1] = float(sq[1])/2 - (float(s[1])/2)**2
-	v[2] = float(sq[2])/2 - (float(s[2])/2)**2
-	
-	#print "The variance:"
-	#print v
-	#print ""
-
-	return v
-
-def checkThreshold(variance, threshold):
-	result = True
-	for i in range(0, len(variance)-1, 1):
-		if variance[i] > threshold[i]:
-			result = False
-			break
-	return result
+	if case == 1:
+		cr["value"] = getClusterIndex(l, mdThre, varThre, case)
+		
+		if cr["value"] == -1: 
+			cr["type"] = 3 			# 3: means the RS
+		else:
+			cr["type"] = 1
+	if case == 2:
+		cr["value"] = getClusterIndex(l, mdThre, varThre, case)
+		if cr["value"] == -1: 		# If the point does not belong to any clusters, it would be summarized into RS Set
+			cr["type"] = 3
+		else:
+			cr["type"] = 1
+	if case == 3:
+		cr["value"] = getClusterIndex(l, mdThre, varThre, case)
+		if cr["value"] == -1: 		# If the point does not belong to any clusters, it would be summarized into RS Set
+			cr["type"] = 3
+		else:
+			cr["type"] = 1
+	return cr
 
 def addToDS(item, cluster):
 	cluster["SUMSQ"][0] = float(cluster["SUMSQ"][0]) + getSUMSQ(item["lati"])
@@ -135,121 +187,34 @@ def addToDS(item, cluster):
 	cluster["Points"].append(item)
 	return cluster
 
-def addToRS(item, ):
-	pass
-
-
-def getClusterIndex(l, t, tList, type):
-	if type == 1:
-		index = -1 
-		result = 0
-		r = True
-		for j in range(0, len(l), 1):
-			for i in range(0, len(l[j]["value"]), 1):
-				if l[j]["value"][i] > tList[i]:
-					r = False
-					break
-			if r == True:
-				index = j
-				return index
-				break
-		return index
-	if type == 2:
-		index = -1
-		result = t
-		for z in range(0, len(l), 1):
-			if l[z]["value"] <= result:
-				index = z
-				result = l[z]["value"]
-		return index
-	if type == 3:
-		index = -1
-		result = t
-		for z in range(0, len(l), 1):
-			if l[z]["type"] == 2:
-				if l[z]["value"] <= result:
-					index = z
-					result = l[z]["value"]
-		if index == -1:
-			r = True
-			for j in range(0, len(l), 1):
-				if l[j]["type"] == 1:
-					for i in range(0, len(l[j]["value"]), 1):
-						if l[j]["value"][i] > tList[i]:
-							r = False
-							break
-					if r == True:
-						index = j
-						return index
-			return index
-		else:
-			return index
-		
-def getClusterResult(l, t, tList, k):
-	cr = {}
-	case = 0 # 1: all are variance, 2: all are md, 3: some are md , some are variance
-	t1 = 0
-	t2 = 0
-	for i in range(0, len(l), 1):
-		if l[i]["type"] == 1: # mean variance 
-			t1 = t1 + 1
-		if l[i]["type"] == 2:
-			t2 = t2 + 1
-	
-	if t1 == k :
-		case = 1
-	if t2 == k:
-		case = 2
-	if 0 < t1 < k:
-		case = 3
-
-	
-	if case == 1:
-		cr["value"] = getClusterIndex(l, t, tList, case)
-		cr["type"] = 1
-	if case == 2:
-		cr["value"] = getClusterIndex(l, t, tList, case)
-		cr["type"] = 1
-	if case == 3:
-		cr["type"] = 1
-		cr["value"] = getClusterIndex(l, t, tList, case)
-		
-	return cr
-
 for i in range(0, len(dataList), 1):
 	for j in range(0, len(dsList), 1):
-		if dsList[j]["N"] < 2:  # if N < 2, then use Threshol
-			td = getTVariance(dataList[i], dsList[j])
-			resultList.append({"type":1, "value": td})
-
-		else:					# if N > 1, then use md
+		
+		if dsList[j]["N"] < 2:  								# if N < 2, then use variance
+			td = getCombinedVariance(dataList[i], dsList[j])	# get the combined variance
+			resultList.append({"type":1, "value": td})			# store in the result list, type 1 means combined variance
+		else:													# if N > 1, then use md
 			variance = getVariance(dsList[j])
-			md = getMahalanobis(dataList[i],centroidList[j], variance)
-			resultList.append({"type":2, "value":md})
+			md = getMahalanobis(dataList[i],centroidList[j], variance)	# get the MD
+			resultList.append({"type":2, "value":md})					# store the result, type 2 means the MD
 			
-	if(len(resultList) > 0):
-		clusterResult = getClusterResult(resultList, mdThreshold, threshold, k)
-		if clusterResult["type"] == 1 :
-			if clusterResult["value"] == -1:
-				rsList.append(dataList[i])
-			else:
+	if(len(resultList) > 0):											# start to clustering, chech which cluster the point belongs to
+		
+		clusterResult = getClusterResult(resultList, mdThreshold, threshold, k)	# get the clustering result
+		if clusterResult["type"] == 1 :											# belongs to a cluster (DS Set)
+			
 				dsList[clusterResult["value"]] = addToDS(dataList[i], dsList[clusterResult["value"]],)
-		if clusterResult["type"] == 2:
-			pass
-	resultList = []
-	clusterResult = {}
+		if clusterResult["type"] == 3:										
+			rsList.append(dataList[i])
+
+	
+	resultList = []												# clear the list for the next point
+	clusterResult = {}											# clear the dict for the next point
 
 for i in range(0, len(dsList), 1):
 	print "cluster", i + 1, ":", dsList[i]["N"]	
 
 print "the number of points in RS:", len(rsList)
-
-def CS():
-	pass
-
-def RS():
-	pass
-
 
 
 
